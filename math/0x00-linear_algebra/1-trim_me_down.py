a305b5dcb1f6560e9b6cf36389f831a4f448d3e7
Isaac <zuderg@gmail.com>
2022-02-04 20:27:47 +0000

    unpycodestyled

--- math/0x00-linear_algebra/1-trim_me_down.py
+++ math/0x00-linear_algebra/1-trim_me_down.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-"""returns middle columns of a python 2D Array"""
 matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
 the_middle = []
 for i in range(len(matrix)):
