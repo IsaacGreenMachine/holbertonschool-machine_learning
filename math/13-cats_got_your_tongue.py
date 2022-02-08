4e46135e0dcab25f689cc66125e3a14dc54c4841
Isaac Green <zuderg@gmail.com>
2022-02-08 11:49:05 -0600

    Delete 13-cats_got_your_tongue.py

--- math/13-cats_got_your_tongue.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""file for np_cat function"""
-import numpy as np
-
-
-def np_cat(mat1, mat2, axis=0):
-    """returns concat of two numpy matrices"""
-    return np.concatenate((mat1, mat2), axis=axis)
