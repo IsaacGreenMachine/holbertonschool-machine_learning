af0b7ee21f1523050a0b466eda815990d5c557c8
Isaac <zuderg@gmail.com>
2022-02-08 03:32:10 +0000

    added numpy version

---
+++ math/0x00-linear_algebra/101-the_whole_barn.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import numpy as np
+"""file for add_matrices"""
+
+
+def add_matrices(mat1, mat2):
+    """implements numpy add matrices"""
+    m1 = np.array(mat1)
+    m2 = np.array(mat2)
+    if m1.shape == m2.shape:
+        return (m1 + m2).tolist()
+    else:
+        return None
+    '''
+    mat1s = matrix_shape(mat1)
+    mat2s = matrix_shape(mat2)
+    if mat1s == mat2s:
+        for i
+
+    else:
+        return None
+'''
+
+
+def matrix_shape(matrix):
+    """returns a list with dimensions of given matrix"""
+    # returns a list with the current dimension + all deeper dimensions.
+    try:
+        return [len(matrix)] + matrix_shape(matrix[0])
+    # this happens when no deeper level of a matrix is available.
+    except Exception:
+        return []
