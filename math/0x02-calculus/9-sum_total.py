ab09953ad031085c6c21e197846d697b5ef4a79a
Isaac Green <zuderg@gmail.com>
2022-02-12 20:33:09 -0600

    added fringe case

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -4,6 +4,9 @@
 
 def summation_i_squared(n):
     """applies summation of i^2 from 0 to n"""
-    n2 = list(range(1, n+1))
-    n2 = list(map(lambda n: n**2, n2))
-    return sum(n2)
+    if n < 1:
+        return None
+    else:
+        n2 = list(range(1, n+1))
+        n2 = list(map(lambda n: n**2, n2))
+        return sum(n2)
