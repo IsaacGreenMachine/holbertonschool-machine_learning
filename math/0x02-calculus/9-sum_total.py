4e0df19722510b39c775df5bfdb64f5373e9c2b9
Isaac Green <zuderg@gmail.com>
2022-02-12 20:32:13 -0600

    removed loop

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -4,10 +4,6 @@
 
 def summation_i_squared(n):
     """applies summation of i^2 from 0 to n"""
-    if n < 1:
-        return None
-    else:
-        sum = 0
-        for i in range(1, n+1):
-            sum += (i)**2
-        return sum
+    n2 = list(range(1, n+1))
+    n2 = list(map(lambda n: n**2, n2))
+    return sum(n2)
