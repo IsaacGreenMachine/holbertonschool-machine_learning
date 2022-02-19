d234fb2e9fa4a57b8a70c4204fc6de357d54cc0d
Isaac Green <zuderg@gmail.com>
2022-02-18 19:34:24 -0600

    added binomial

--- math/0x03-probability/binomial.py
+++ math/0x03-probability/binomial.py
@@ -4,6 +4,7 @@
 
 class Binomial():
     """implements a class binomial distribution capable of stat functions"""
+    '''
     def __init__(self, data=None, n=1, p=0.5):
         """sets n and p based off of mean and variance of data"""
         if data is None:
@@ -46,6 +47,16 @@ class Binomial():
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
+            total = sum(data)
+            leng = len(data)
+            mean = total/leng  # = np
+            var = sum([(i - mean)**2 for i in data])/leng  # = npq
+            q_est = var / mean
+            p_est = 1 - q_est
+            n_est = (total/p_est) / leng  # " total is p% of n " formula
+            self.n = int(round(n_est))
+            self.p = float(mean/self.n)
+    '''
             nns = []
             mean = sum(data) / len(data)
             v = sum([(i - mean) ** 2 for i in data]) / len(data)
