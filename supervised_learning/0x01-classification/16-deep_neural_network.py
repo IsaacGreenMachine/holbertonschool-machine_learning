565686c43749516ae4dce8a320c5e442e0a71969
Isaac Green <zuderg@gmail.com>
2022-03-07 21:16:19 -0600

    trying other code

--- supervised_learning/0x01-classification/16-deep_neural_network.py
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -1,39 +1,36 @@
 #!/usr/bin/env python3
-"""Module containing the class DeepNeuralNetwork which defines a deep neural
-network performing binary classification."""
+"""Deep Neural Network Module"""
+
+
 import numpy as np
 
 
 class DeepNeuralNetwork():
-    """Class that defines a deep neural network performing binary
-    classification."""
+    """
+       Deep Neural Network Class, used for binary
+       classification on handwritten digits
+    """
 
     def __init__(self, nx, layers):
-        """Class constructor
-        Args:
-            nx (int): The number of input features
-            layers (list[int]): List representing the number of nodes in each
-                layer of the network
-        """
-        if not isinstance(nx, int):
+        """init method for DeepNeuralNetwork class"""
+        if type(nx) is not int:
             raise TypeError("nx must be an integer")
-        elif nx < 1:
+        if nx < 1:
             raise ValueError("nx must be a positive integer")
-        elif not isinstance(layers, list) or len(layers) < 1:
+        if type(layers) is not list or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-
+        if False in (np.array(layers) > 0):
+            raise TypeError("layers must be a list of positive integers")
         self.L = len(layers)
         self.cache = {}
-        weights = {}
-        prev = nx
-        for i, l in enumerate(layers, 1):
-            if not isinstance(l, int) or l < 1:
-                raise TypeError("layers must be a list of positive integers")
-            weights["b{}".format(i)] = np.zeros((l, 1))
-            weights["W{}".format(i)] = (np.random.randn(l, prev) *
-                                        np.sqrt(2 / prev))
-            prev = l
-        self.weights = weights
+        self.weights = {}
+        layers = [nx] + layers
+        for x, l in enumerate(layers[1:], start=1):
+            self.weights["W{}".format(x)] = (
+                np.random.randn(l, layers[x - 1]) *
+                np.sqrt(2/(layers[x - 1]))
+            )
+            self.weights["b{}".format(x)] = np.zeros((l, 1))
 
 
 '''
