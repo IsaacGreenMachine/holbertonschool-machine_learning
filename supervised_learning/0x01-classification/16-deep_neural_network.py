c9449c4cfd280a9cbd7f227652b77d693970251a
Isaac Green <zuderg@gmail.com>
2022-03-07 21:11:33 -0600

    trying other code

--- supervised_learning/0x01-classification/16-deep_neural_network.py
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -1,3 +1,41 @@
+"""Module containing the class DeepNeuralNetwork which defines a deep neural
+network performing binary classification."""
+import numpy as np
+
+
+class DeepNeuralNetwork():
+    """Class that defines a deep neural network performing binary
+    classification."""
+
+    def __init__(self, nx, layers):
+        """Class constructor
+        Args:
+            nx (int): The number of input features
+            layers (list[int]): List representing the number of nodes in each
+                layer of the network
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        elif nx < 1:
+            raise ValueError("nx must be a positive integer")
+        elif not isinstance(layers, list) or len(layers) < 1:
+            raise TypeError("layers must be a list of positive integers")
+
+        self.L = len(layers)
+        self.cache = {}
+        weights = {}
+        prev = nx
+        for i, l in enumerate(layers, 1):
+            if not isinstance(l, int) or l < 1:
+                raise TypeError("layers must be a list of positive integers")
+            weights["b{}".format(i)] = np.zeros((l, 1))
+            weights["W{}".format(i)] = (np.random.randn(l, prev) *
+                                        np.sqrt(2 / prev))
+            prev = l
+        self.weights = weights
+
+
+'''
 #!/usr/bin/env python3
 """module for deep neural network class"""
 import numpy as np
@@ -24,3 +62,4 @@ class DeepNeuralNetwork:
             rnd_val = np.random.randn(layers[layer], layers[layer - 1]) * he
             self.weights["W{}".format(layer)] = rnd_val
             self.weights["b{}".format(layer)] = np.zeros((layers[layer], 1))
+'''
