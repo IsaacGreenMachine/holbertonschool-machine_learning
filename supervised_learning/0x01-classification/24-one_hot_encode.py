91a37ba72cdd66831dcf9a5761adb825a1865fec
Isaac Green <zuderg@gmail.com>
2022-03-07 21:46:39 -0600

    fringe cases

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -5,6 +5,10 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """encodes a given vector to one-hot vector"""
+    if type(Y) is not np.ndarray or type(classes) is not int:
+        return None
+    if classes < 2 or classes < np.amax(Y):
+        return None
     oh = np.zeros((classes, Y.size))
     oh[Y, np.arange(Y.size)] = 1
     return oh
