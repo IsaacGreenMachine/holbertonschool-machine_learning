6f74bf86966768ce32725d11f096f5aabb04feec
Isaac Green <zuderg@gmail.com>
2022-03-09 14:44:21 -0600

    adding task 0

--- supervised_learning/0x00-tensorflow/0-create_placeholders.py
+++ supervised_learning/0x00-tensorflow/0-create_placeholders.py
@@ -1,14 +1,13 @@
 import numpy as np
 import tensorflow as tf
+
+
 def create_placeholders(nx, classes):
-    pass
-
-a = tf.constant(3.0, dtype=tf.float32)
-b = tf.constant(4.0) # also tf.float32 implicitly
-total = a + b
-print(a)
-print(b)
-print(total)
-
-sess = tf.Session()
-print(sess.run(total))
+    """
+    returns placeholders
+    nx: data feature columns
+    classes: num classes in classifier
+    """
+    x = tf.placeholder(tf.float32, shape=(None, nx), name="x")
+    y = tf.placeholder(tf.float32, shape=(None, classes), name="y")
+    return x, y
