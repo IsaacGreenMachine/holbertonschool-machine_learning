31ce78213d9ac7d7733b601e18fedaa75251176b
Isaac Green <zuderg@gmail.com>
2022-03-14 22:56:53 -0500

    returning individaul items rather than list

--- supervised_learning/0x00-tensorflow/7-evaluate.py
+++ supervised_learning/0x00-tensorflow/7-evaluate.py
@@ -13,17 +13,5 @@ def evaluate(X, Y, save_path):
     pred = tf.get_collection('y_pred')[0]
     acc = tf.get_collection('accuracy')[0]
     loss = tf.get_collection('loss')[0]
-    return sess.run([pred, acc, loss], feed_dict={x: X, y: Y})[0:3]
-
-    """
-    with tf.Session() as sess:
-        saver = tf.train.import_meta_graph(save_path + '.meta')
-        saver.restore(sess, save_path)
-        x = tf.get_collection('x')[0]
-        y = tf.get_collection("y")[0]
-        y_pred = tf.get_collection("y_pred")[0]
-        loss = tf.get_collection("loss")[0]
-        acc = tf.get_collection("accuracy")[0]
-        vars = sess.run([y_pred, acc, loss], feed_dict={x: X, y: Y})
-        return vars[0], vars[1], vars[2]
-    """
+    vars = sess.run([pred, acc, loss], feed_dict={x: X, y: Y})
+    return vars[0], vars[1], vars[2]
