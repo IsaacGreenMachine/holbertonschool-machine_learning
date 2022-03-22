f05a6b46b18576aea7d2482dd5cca815ea7cb798
Isaac Green <zuderg@gmail.com>
2022-03-21 23:02:35 -0500

    PEMDAS

--- supervised_learning/0x01-optimization/11-learning_rate_decay.py
+++ supervised_learning/0x01-optimization/11-learning_rate_decay.py
@@ -4,4 +4,4 @@
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """updates the learning rate using inverse time decay"""
-    return alpha / (1 + decay_rate * global_step // decay_step)
+    return alpha / (1 + (decay_rate * (global_step // decay_step)))
