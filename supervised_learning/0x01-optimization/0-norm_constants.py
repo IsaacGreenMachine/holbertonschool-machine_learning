765342b7684c61f1ced8295c9e82f270f73f4818
Isaac Green <zuderg@gmail.com>
2022-03-21 23:00:25 -0500

    removed print statement

--- supervised_learning/0x01-optimization/0-norm_constants.py
+++ supervised_learning/0x01-optimization/0-norm_constants.py
@@ -5,7 +5,6 @@ import numpy as np
 
 def normalization_constants(X):
     """calculates the normalization (standardization) constants of a matrix"""
-    print(X.shape)
     m = np.sum(X, axis=0) / X.shape[0]
     s = np.sqrt(np.sum((X - m)**2, axis=0) / X.shape[0])
     return (m, s)
