ea14a0288a52fb361db6888f5c60e9bc315fb7d8
Isaac Green <zuderg@gmail.com>
2022-03-23 23:28:58 -0500

    adding imports

--- supervised_learning/0x00-error_analysis/4-f1_score.py
+++ supervised_learning/0x00-error_analysis/4-f1_score.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """module for f1_score"""
 import numpy as np
+sensitivity = __import__('1-sensitivity').sensitivity
+precision = __import__('2-precision').precision
 
 
 def f1_score(confusion):
@@ -8,6 +10,6 @@ def f1_score(confusion):
     tru_pos = np.diag(confusion)
     fals_pos = np.sum(confusion, axis=0) - tru_pos
     fals_neg = np.sum(confusion, axis=1) - tru_pos
-    precision = tru_pos / (tru_pos + fals_pos)
+    precis = precision(confusion)
     recall = tru_pos / (tru_pos + fals_neg)
-    return 2/((recall ** -1) + (precision ** -1))
+    return 2/((recall ** -1) + (precis ** -1))
