74ccaf842ea7d2086d0cc59d0f0cf56cfadb3ea8
Isaac Green <zuderg@gmail.com>
2022-03-28 15:15:22 -0500

    doing breakout after activation

--- supervised_learning/0x01-regularization/4-dropout_forward_prop.py
+++ supervised_learning/0x01-regularization/4-dropout_forward_prop.py
@@ -20,24 +20,29 @@ def dropout_forward_prop(X, weights, L, keep_prob):
     Returns: dictionary of layer outputs and layer dropout masks
     """
     outputs = {}
-    outputs.update({"A0": X})
+    outputs["A0"] = X
     for layer in range(1, L+1):
         w = weights["W{}".format(layer)]
         a = outputs["A{}".format(layer-1)]
+        b = weights["b{}".format(layer)]
+        z = np.matmul(w, a) + b
 
-        if layer < L:
+        # last layer
+        if layer == L:
+            t = np.exp(z)
+            outputs["A{}".format(layer)] = t/np.sum(t, axis=0)
+
+        # hidden layers
+        else:
+            top = np.exp(z) - np.exp(-z)
+            bot = np.exp(z) + np.exp(-z)
+            a = top / bot
+
+            # dropout
             dx = np.random.rand(a.shape[0], a.shape[1]) < keep_prob
             outputs["D{}".format(layer)] = dx*1
             a *= dx
             a /= keep_prob
+            outputs["A{}".format(layer)] = a
 
-        b = weights["b{}".format(layer)]
-        z = np.matmul(w, a) + b
-        if layer == L:
-            t = np.exp(z)
-            outputs["A{}".format(layer)] = t/np.sum(t, axis=0)
-        else:
-            top = np.exp(z) - np.exp(-z)
-            bot = np.exp(z) + np.exp(-z)
-            outputs["A{}".format(layer)] = top / bot
     return outputs
