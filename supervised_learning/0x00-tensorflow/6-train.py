2f35f51f75f2723901c7d8cde4721d8b069b31e9
Isaac Green <zuderg@gmail.com>
2022-03-29 12:06:19 -0500

    revamping

--- supervised_learning/0x00-tensorflow/6-train.py
+++ supervised_learning/0x00-tensorflow/6-train.py
@@ -18,27 +18,27 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
     loss = calculate_loss(y, y_pred)
     accuracy = calculate_accuracy(y, y_pred)
     train_op = create_train_op(loss, alpha)
-    init = tf.global_variables_initializer()
     tf.add_to_collection('x', x)
     tf.add_to_collection('y', y)
     tf.add_to_collection('y_pred', y_pred)
     tf.add_to_collection('loss', loss)
     tf.add_to_collection('accuracy', accuracy)
-    tf.add_to_collection('train_op', train_op)
+    tf.add_to_collection("train_op", train_op)
+    init = tf.global_variables_initializer()
     with tf.Session() as sess:
         sess.run(init)
-        for i in range(iterations+1):
+        for i in range(iterations + 1):
             if i % 100 == 0 or i == iterations:
-                tLoss, tAccuracy = sess.run((loss, accuracy),
-                                            feed_dict={x: X_train, y: Y_train})
-                vLoss, vAccuracy = sess.run((loss, accuracy),
-                                            feed_dict={x: X_valid, y: Y_valid})
                 print("After {} iterations:".format(i))
-                print("\tTraining Cost: {}".format(tLoss))
-                print("\tTraining Accuracy: {}".format(tAccuracy))
-                print("\tValidation Cost: {}".format(vLoss))
-                print("\tValidation Accuracy: {}".format(vAccuracy))
+                tloss, taccuracy = sess.run([loss, accuracy],
+                                            feed_dict={x: X_train, y: Y_train})
+                print("\tTraining Cost: {}".format(tloss))
+                print("\tTraining Accuracy: {}".format(taccuracy))
+                vloss, vaccuracy = sess.run([loss, accuracy],
+                                            feed_dict={x: X_valid, y: Y_valid})
+                print("\tValidation Cost: {}".format(vloss))
+                print("\tValidation Accuracy: {}".format(vaccuracy))
             if i < iterations:
-                sess.run((train_op), feed_dict={x: X_train, y: Y_train})
+                sess.run(train_op, feed_dict={x: X_train, y: Y_train})
         saver = tf.train.Saver()
         return saver.save(sess, save_path)
