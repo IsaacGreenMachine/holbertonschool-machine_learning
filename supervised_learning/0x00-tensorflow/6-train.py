f05d2172483f5a530daea7ab4986e2486eed4764
Isaac Green <zuderg@gmail.com>
2022-03-29 12:08:10 -0500

    new code

--- supervised_learning/0x00-tensorflow/6-train.py
+++ supervised_learning/0x00-tensorflow/6-train.py
@@ -1,28 +1,31 @@
 #!/usr/bin/env python3
-"""module for train function"""
+"""Train classification model"""
 
 
 import tensorflow as tf
-calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
-calculate_loss = __import__('4-calculate_loss').calculate_loss
+
+
 create_placeholders = __import__('0-create_placeholders').create_placeholders
-create_train_op = __import__('5-create_train_op').create_train_op
+calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
 forward_prop = __import__('2-forward_prop').forward_prop
+calculate_loss = __import__('4-calculate_loss').calculate_loss
+create_train_op = __import__('5-create_train_op').create_train_op
 
 
-def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
-          activations, alpha, iterations, save_path="/tmp/model.ckpt"):
-    """creates and trains a tf neural net for x iterations, saves tf format"""
+def train(X_train, Y_train, X_valid,
+          Y_valid, layer_sizes, activations, alpha,
+          iterations, save_path="/tmp/model.ckpt"):
+    """Train Classification Model"""
     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
-    y_pred = forward_prop(x, layer_sizes, activations)
-    loss = calculate_loss(y, y_pred)
-    accuracy = calculate_accuracy(y, y_pred)
-    train_op = create_train_op(loss, alpha)
     tf.add_to_collection('x', x)
     tf.add_to_collection('y', y)
+    y_pred = forward_prop(x, layer_sizes, activations)
     tf.add_to_collection('y_pred', y_pred)
+    loss = calculate_loss(y, y_pred)
     tf.add_to_collection('loss', loss)
+    accuracy = calculate_accuracy(y, y_pred)
     tf.add_to_collection('accuracy', accuracy)
+    train_op = create_train_op(loss, alpha)
     tf.add_to_collection("train_op", train_op)
     init = tf.global_variables_initializer()
     with tf.Session() as sess:
