65d74d8436196def3c53031938da36cc4788ab33
Isaac Green <zuderg@gmail.com>
2022-03-29 11:59:27 -0500

    why no output?

--- supervised_learning/0x00-tensorflow/6-train.py
+++ supervised_learning/0x00-tensorflow/6-train.py
@@ -10,9 +10,8 @@ forward_prop = __import__('2-forward_prop').forward_prop
 
 def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
           activations, alpha, iterations, save_path="/tmp/model.ckpt"):
-    """trains a tf neural net for x iterations, saves tf format"""
+    """creates and trains a tf neural net for x iterations, saves tf format"""
     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
-    sess = tf.Session()
     y_pred = forward_prop(x, layer_sizes, activations)
     loss = calculate_loss(y, y_pred)
     accuracy = calculate_accuracy(y, y_pred)
@@ -24,19 +23,20 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
     tf.add_to_collection('loss', loss)
     tf.add_to_collection('accuracy', accuracy)
     tf.add_to_collection('train_op', train_op)
-    sess.run(init)
-    for i in range(iterations+1):
-        if i % 100 == 0 or i == iterations:
-            tLoss, tAccuracy = sess.run((loss, accuracy),
-                                        feed_dict={x: X_train, y:  Y_train})
-            vLoss, vAccuracy = sess.run((loss, accuracy),
-                                        feed_dict={x: X_valid, y: Y_valid})
-            print("After {} iterations:".format(i))
-            print("\tTraining Cost: {}".format(tLoss))
-            print("\tTraining Accuracy: {}".format(tAccuracy))
-            print("\tValidation Cost: {}".format(vLoss))
-            print("\tValidation Accuracy: {}".format(vAccuracy))
-        if i < iterations:
-            sess.run((train_op), feed_dict={x: X_train, y: Y_train})
-    saver = tf.train.Saver()
-    return saver.save(sess, save_path)
+    with tf.Session() as sess:
+        sess.run(init)
+        for i in range(iterations+1):
+            if i % 100 == 0 or i == iterations:
+                tLoss, tAccuracy = sess.run((loss, accuracy),
+                                            feed_dict={x: X_train, y: Y_train})
+                vLoss, vAccuracy = sess.run((loss, accuracy),
+                                            feed_dict={x: X_valid, y: Y_valid})
+                print("After {} iterations:".format(i))
+                print("\tTraining Cost: {}".format(tLoss))
+                print("\tTraining Accuracy: {}".format(tAccuracy))
+                print("\tValidation Cost: {}".format(vLoss))
+                print("\tValidation Accuracy: {}".format(vAccuracy))
+            if i < iterations:
+                sess.run((train_op), feed_dict={x: X_train, y: Y_train})
+        saver = tf.train.Saver()
+        return saver.save(sess, save_path)
