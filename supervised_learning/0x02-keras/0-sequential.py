eed7f363cae0ba23023d7123163ecf1c10d6cde3
Isaac Green <zuderg@gmail.com>
2022-03-31 23:36:26 -0500

    fixing order of dropout and dense layers

--- supervised_learning/0x02-keras/0-sequential.py
+++ supervised_learning/0x02-keras/0-sequential.py
@@ -20,15 +20,13 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
                              kernel_regularizer=K.regularizers.l2(lambtha),
                              input_shape=(nx,)
                              ))
-    model.add(K.layers.Dropout(1 - keep_prob))
 
     for i in range(len(layers)):
+        model.add(K.layers.Dropout(1 - keep_prob))
         model.add(K.layers.Dense(
             layers[i],
             activations[i],
             kernel_regularizer=K.regularizers.l2(lambtha)
             )
             )
-        if i < len(layers) - 1:
-            model.add(K.layers.Dropout(1 - keep_prob))
     return model
