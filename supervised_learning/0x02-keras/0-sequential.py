66eac35dcfc4f9d9d1be64c33e996847edb0acbb
Isaac Green <zuderg@gmail.com>
2022-03-31 23:37:42 -0500

    fixing iterator

--- supervised_learning/0x02-keras/0-sequential.py
+++ supervised_learning/0x02-keras/0-sequential.py
@@ -21,7 +21,7 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
                              input_shape=(nx,)
                              ))
 
-    for i in range(len(layers)):
+    for i in range(1, len(layers)):
         model.add(K.layers.Dropout(1 - keep_prob))
         model.add(K.layers.Dense(
             layers[i],
