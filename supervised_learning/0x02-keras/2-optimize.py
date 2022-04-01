47431a1e9e736824d2773ab5d30366d5b563a61b
Isaac Green <zuderg@gmail.com>
2022-03-31 23:39:41 -0500

    changing name for learning rate

--- supervised_learning/0x02-keras/2-optimize.py
+++ supervised_learning/0x02-keras/2-optimize.py
@@ -14,7 +14,7 @@ def optimize_model(network, alpha, beta1, beta2):
     beta2 - second Adam optimization parameter
     Returns: None
     """
-    opt = K.optimizers.Adam(learning_rate=alpha, beta_1=beta1, beta_2=beta2)
+    opt = K.optimizers.Adam(lr=alpha, beta_1=beta1, beta_2=beta2)
     network.compile(loss="categorical_crossentropy",
                     optimizer=opt, metrics=['accuracy'])
     return None
