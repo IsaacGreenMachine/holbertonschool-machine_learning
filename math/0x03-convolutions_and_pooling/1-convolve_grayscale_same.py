f8c2f6c605cabffde12c19499365f4bc307e714b
Isaac Green <zuderg@gmail.com>
2022-04-05 22:25:31 -0500

    switching y and x for no reason

--- math/0x03-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x03-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -19,11 +19,14 @@ def convolve_grayscale_same(images, kernel):
 
     Returns: numpy.ndarray containing the convolved images
     """
-    images = np.pad(images, ((0, 0), (1, 1), (1, 1)))
-    conv_size = images.shape[1]-kernel.shape[0]+1
-    conv = np.ndarray((images.shape[0], conv_size, conv_size))
-    for x in range(conv_size):
-        for y in range(conv_size):
+    conv_size_h = images.shape[1]-kernel.shape[0]+1
+    conv_size_w = images.shape[2]-kernel.shape[1]+1
+    pad_h = kernel.shape[0] // 2
+    pad_w = kernel.shape[1] // 2
+    images = np.pad(images, ((0, 0), (pad_h, pad_h), (pad_w, pad_w)))
+    conv = np.ndarray((images.shape[0], conv_size_h, conv_size_w))
+    for x in range(conv_size_h):
+        for y in range(conv_size_w):
             conv[:, x, y] = np.sum(images[:,
                                           x:x+kernel.shape[0],
                                           y:y+kernel.shape[1]] * kernel,
