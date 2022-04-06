c118f84386d82da5813cf5b0c34e517f6fc0a52b
Isaac Green <zuderg@gmail.com>
2022-04-05 22:19:27 -0500

    switching for no reason

--- math/0x03-convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/0x03-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -19,32 +19,15 @@ def convolve_grayscale_valid(images, kernel):
     Returns: numpy.ndarray containing the convolved images
     """
 
-    '''
     conv_size_h = images.shape[1]-kernel.shape[0]+1
     conv_size_w = images.shape[2]-kernel.shape[1]+1
     conv = np.zeros((images.shape[0], conv_size_h, conv_size_w))
 
-    for x in range(conv_size_w):
-        for y in range(conv_size_h):
+    for x in range(conv_size_h):
+        for y in range(conv_size_w):
             conv[:, x, y] = np.sum(images[:,
                                           x:x+kernel.shape[1],
                                           y:y+kernel.shape[0]
                                           ] * kernel,
                                    axis=(1, 2))
-    return conv
-
-    '''
-    im, ih, iw = images.shape
-    kh, kw = kernel.shape
-
-    dim_h = ih - kh + 1
-    dim_w = iw - kw + 1
-
-    conv = np.zeros((im, dim_h, dim_w))
-
-    for w in range(dim_h):
-        for h in range(dim_w):
-            conv[:, w, h] = (
-                kernel * images[:, w: w+kw, h:h+kh]
-                ).sum(axis=(1, 2))
     return conv
