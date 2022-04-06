1b4b712031709f695f1999bd41af714ef2ce635c
Isaac Green <zuderg@gmail.com>
2022-04-05 22:13:09 -0500

    changing description

--- math/0x03-convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/0x03-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""module for convolve_grayscale_valid"""
+"""module for convolve_grayscale_valid(images, kernel)"""
 import numpy as np
 
 
@@ -18,18 +18,20 @@ def convolve_grayscale_valid(images, kernel):
 
     Returns: numpy.ndarray containing the convolved images
     """
-    '''
-    conv_size = images.shape[1]-kernel.shape[0]+1
-    conv = np.zeros((images.shape[0], conv_size, conv_size))
 
-    for x in range(conv_size):
-        for y in range(conv_size):
+    conv_size_h = images.shape[1]-kernel.shape[0]+1
+    conv_size_w = images.shape[2]-kernel.shape[1]+1
+    conv = np.zeros((images.shape[0], conv_size_h, conv_size_w))
+
+    for x in range(conv_size_w):
+        for y in range(conv_size_h):
             conv[:, x, y] = np.sum(images[:,
-                                          x:x+kernel.shape[0],
-                                          y:y+kernel.shape[1]
+                                          x:x+kernel.shape[1],
+                                          y:y+kernel.shape[0]
                                           ] * kernel,
                                    axis=(1, 2))
     return conv
+
     '''
     samples, samp_h, samp_w = images.shape
     filter_h, filter_w = kernel.shape
@@ -45,3 +47,4 @@ def convolve_grayscale_valid(images, kernel):
                 kernel * images[:, w: w+filter_w, h:h+filter_h]
                 ).sum(axis=(1, 2))
     return conv
+    '''
