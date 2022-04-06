2dbc0c8c9dac1b235454cc53ecdbf5d7a6c26dc9
Isaac Green <zuderg@gmail.com>
2022-04-05 22:05:57 -0500

    ndarray init -> np.zeros

--- math/0x03-convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/0x03-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -19,13 +19,13 @@ def convolve_grayscale_valid(images, kernel):
     Returns: numpy.ndarray containing the convolved images
     """
     conv_size = images.shape[1]-kernel.shape[0]+1
-    conv = np.ndarray((images.shape[0], conv_size, conv_size))
+    conv = np.zeros((images.shape[0], conv_size, conv_size))
 
     for x in range(conv_size):
         for y in range(conv_size):
-            conv[:, y, x] = np.sum(images[:,
-                                          y:y+kernel.shape[1],
-                                          x:x+kernel.shape[0]
+            conv[:, x, y] = np.sum(images[:,
+                                          x:x+kernel.shape[0],
+                                          y:y+kernel.shape[1]
                                           ] * kernel,
                                    axis=(1, 2))
     return conv
