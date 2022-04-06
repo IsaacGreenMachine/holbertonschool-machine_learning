331d124d16fc71fd18277a2aa885ad7edc71cb4f
Isaac Green <zuderg@gmail.com>
2022-04-05 22:07:04 -0500

    tryinbg

--- math/0x03-convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/0x03-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -18,6 +18,7 @@ def convolve_grayscale_valid(images, kernel):
 
     Returns: numpy.ndarray containing the convolved images
     """
+    '''
     conv_size = images.shape[1]-kernel.shape[0]+1
     conv = np.zeros((images.shape[0], conv_size, conv_size))
 
@@ -28,4 +29,19 @@ def convolve_grayscale_valid(images, kernel):
                                           y:y+kernel.shape[1]
                                           ] * kernel,
                                    axis=(1, 2))
+    return conv
+    '''
+    samples, samp_h, samp_w = images.shape
+    filter_h, filter_w = kernel.shape
+
+    dim_h = samp_h - filter_h + 1
+    dim_w = samp_w - filter_w + 1
+
+    conv = np.zeros((samples, dim_h, dim_w))
+
+    for w in range(dim_h):
+        for h in range(dim_w):
+            conv[:, w, h] = (
+                kernel * images[:, w: w+filter_w, h:h+filter_h]
+                ).sum(axis=(1, 2))
     return conv
