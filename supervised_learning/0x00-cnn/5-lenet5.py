ab05edc6d9c56a3e7e7453de8619b20dac5b172d
Isaac Green <zuderg@gmail.com>
2022-04-13 18:51:00 -0500

    trying model instead of sequential

--- supervised_learning/0x00-cnn/5-lenet5.py
+++ supervised_learning/0x00-cnn/5-lenet5.py
@@ -15,6 +15,7 @@ def lenet5(X):
         - accuracy metrics
     """
 
+    """
     model = K.Sequential()
 
     # input layer
@@ -67,3 +68,26 @@ def lenet5(X):
                   metrics=['accuracy'])
 
     return model
+    """
+
+    c1 = K.layers.Conv2D(6, kernel_size=(5, 5), padding='same',
+                         activation='relu',
+                         kernel_initializer='HeNormal')(X)
+    p1 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(c1)
+    c2 = K.layers.Conv2D(16, kernel_size=(5, 5), padding='valid',
+                         activation='relu',
+                         kernel_initializer='HeNormal')(p1)
+    p2 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(c2)
+    flat = K.layers.Flatten()(p2)
+    layer1 = K.layers.Dense(units=120, kernel_initializer='HeNormal',
+                            activation='relu')(flat)
+    layer2 = K.layers.Dense(units=84, kernel_initializer='HeNormal',
+                            activation='relu')(layer1)
+    layer3 = K.layers.Dense(units=10, kernel_initializer='HeNormal',
+                            activation='softmax')(layer2)
+    newModel = K.models.Model(X, layer3)
+    adam = K.optimizers.Adam()
+    newModel.compile(optimizer=adam,
+                     loss='categorical_crossentropy',
+                     metrics=['accuracy'])
+    return newModel
