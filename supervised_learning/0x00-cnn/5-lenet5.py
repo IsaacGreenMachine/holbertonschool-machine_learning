e09a49f98be1a1a084a42684a0f57dc8a480c887
Isaac Green <zuderg@gmail.com>
2022-04-13 18:52:40 -0500

    chaning init to he_norm func rather that HeNorm String

--- supervised_learning/0x00-cnn/5-lenet5.py
+++ supervised_learning/0x00-cnn/5-lenet5.py
@@ -70,20 +70,21 @@ def lenet5(X):
     return model
     """
 
+    init = K.initializers.he_normal()
     c1 = K.layers.Conv2D(6, kernel_size=(5, 5), padding='same',
                          activation='relu',
-                         kernel_initializer='HeNormal')(X)
+                         kernel_initializer=init)(X)
     p1 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(c1)
     c2 = K.layers.Conv2D(16, kernel_size=(5, 5), padding='valid',
                          activation='relu',
-                         kernel_initializer='HeNormal')(p1)
+                         kernel_initializer=init)(p1)
     p2 = K.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(c2)
     flat = K.layers.Flatten()(p2)
-    layer1 = K.layers.Dense(units=120, kernel_initializer='HeNormal',
+    layer1 = K.layers.Dense(units=120, kernel_initializer=init,
                             activation='relu')(flat)
-    layer2 = K.layers.Dense(units=84, kernel_initializer='HeNormal',
+    layer2 = K.layers.Dense(units=84, kernel_initializer=init,
                             activation='relu')(layer1)
-    layer3 = K.layers.Dense(units=10, kernel_initializer='HeNormal',
+    layer3 = K.layers.Dense(units=10, kernel_initializer=init,
                             activation='softmax')(layer2)
     newModel = K.models.Model(X, layer3)
     adam = K.optimizers.Adam()
