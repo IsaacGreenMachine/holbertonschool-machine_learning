32aa1ed9dbf4c5f14c02ccf3f7b3fdd01d380eb1
Isaac Green <zuderg@gmail.com>
2022-04-13 19:37:11 -0500

    removing exact dimensions on kernel size

--- supervised_learning/0x00-cnn/4-lenet5.py
+++ supervised_learning/0x00-cnn/4-lenet5.py
@@ -77,16 +77,16 @@ def lenet5(x, y):
 
     init = tf.contrib.layers.variance_scaling_initializer()
     conv1 = tf.layers.Conv2D(
-        filters=6, kernel_size=(5, 5), padding='same',
+        filters=6, kernel_size=5, padding='same',
         activation='relu', kernel_initializer=init
     )(x)
-    pool1 = tf.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv1)
+    pool1 = tf.layers.MaxPooling2D(pool_size=2, strides=(2, 2))(conv1)
 
     conv2 = tf.layers.Conv2D(
-        filters=16, kernel_size=(5, 5), padding='valid',
+        filters=16, kernel_size=5, padding='valid',
         activation='relu', kernel_initializer=init
     )(pool1)
-    pool2 = tf.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv2)
+    pool2 = tf.layers.MaxPooling2D(pool_size=2, strides=2)(conv2)
 
     flat = tf.layers.Flatten()(pool2)
 
