839bdb70974c9bd3f41b5ec6cc6ced96def4d9b5
Isaac Green <zuderg@gmail.com>
2022-04-13 22:41:20 -0500

    added shebang and comments

--- supervised_learning/0x00-cnn/2-conv_backward.py
+++ supervised_learning/0x00-cnn/2-conv_backward.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+"""module for conv_backward"""
+import numpy as np
+
+
 def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
     """performs back propagation over a convolutional layer of a neural network
 
