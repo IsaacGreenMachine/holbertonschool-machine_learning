50f8cefdbcd0641ac8f08c4b6dc2e419f2f11c66
Isaac Green <zuderg@gmail.com>
2022-04-13 19:35:09 -0500

    giving exact dimensions in kernel size

--- supervised_learning/0x00-cnn/4-lenet5.py
+++ supervised_learning/0x00-cnn/4-lenet5.py
@@ -19,6 +19,7 @@ def lenet5(x, y):
     a tensor for the accuracy of the network
     """
 
+    """
     # he_norm weight initializer
     he_norm = tf.initializers.variance_scaling()
 
@@ -72,3 +73,37 @@ def lenet5(x, y):
     acc = tf.reduce_mean(tf.cast(tf.math.equal(y_out, pred_out), tf.float32))
 
     return dense3, opt.minimize(loss), loss, acc
+    """
+
+    init = tf.contrib.layers.variance_scaling_initializer()
+    conv1 = tf.layers.Conv2D(
+        filters=6, kernel_size=(5, 5), padding='same',
+        activation='relu', kernel_initializer=init
+    )(x)
+    pool1 = tf.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv1)
+
+    conv2 = tf.layers.Conv2D(
+        filters=16, kernel_size=(5, 5), padding='valid',
+        activation='relu', kernel_initializer=init
+    )(pool1)
+    pool2 = tf.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv2)
+
+    flat = tf.layers.Flatten()(pool2)
+
+    fc1 = tf.layers.Dense(
+        units=120, kernel_initializer=init, activation='relu'
+        )(flat)
+
+    fc2 = tf.layers.Dense(
+        units=84, kernel_initializer=init, activation='relu'
+        )(fc1)
+
+    fc3 = tf.layers.Dense(units=10, kernel_initializer=init)(fc2)
+    prediction = fc3
+
+    loss = tf.losses.softmax_cross_entropy(y, fc3)
+    Adam = tf.train.AdamOptimizer().minimize(loss)
+    correct = tf.equal(tf.argmax(y, 1), tf.argmax(prediction, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+    softmax = tf.nn.softmax(prediction)
+    return softmax, Adam, loss, accuracy
