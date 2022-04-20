dc90929e9b2f0f1797cf4f8dfdeaa0d3e15e0f29
Isaac Green <zuderg@gmail.com>
2022-04-19 22:36:09 -0500

    changing tuples to lists

--- supervised_learning/0x01-deep_cnns/1-inception_network.py
+++ supervised_learning/0x01-deep_cnns/1-inception_network.py
@@ -146,25 +146,25 @@ def inception_network():
         (3, 3), strides=(2, 2), padding='same'
         )(conv2)
 
-    inception1 = inception_block(pool2, (64, 96, 128, 16, 32, 32))
-    inception2 = inception_block(inception1, (128, 128, 192, 32, 96, 64))
+    inception1 = inception_block(pool2, [64, 96, 128, 16, 32, 32])
+    inception2 = inception_block(inception1, [128, 128, 192, 32, 96, 64])
 
     pool3 = K.layers.MaxPooling2D(
         (3, 3), strides=(2, 2), padding='same'
         )(inception2)
 
-    inception3 = inception_block(pool3, (192, 96, 208, 16, 48, 64))
-    inception4 = inception_block(inception3, (160, 112, 224, 24, 64, 64))
-    inception5 = inception_block(inception4, (128, 128, 256, 24, 64, 64))
-    inception6 = inception_block(inception5, (112, 144, 288, 32, 64, 64))
-    inception7 = inception_block(inception6, (256, 160, 320, 32, 128, 128))
+    inception3 = inception_block(pool3, [192, 96, 208, 16, 48, 64])
+    inception4 = inception_block(inception3, [160, 112, 224, 24, 64, 64])
+    inception5 = inception_block(inception4, [128, 128, 256, 24, 64, 64])
+    inception6 = inception_block(inception5, [112, 144, 288, 32, 64, 64])
+    inception7 = inception_block(inception6, [256, 160, 320, 32, 128, 128])
 
     pool4 = K.layers.MaxPooling2D(
         (3, 3), strides=(2, 2), padding='same'
         )(inception7)
 
-    inception8 = inception_block(pool4, (256, 160, 320, 32, 128, 128))
-    inception9 = inception_block(inception8, (384, 192, 384, 48, 128, 128))
+    inception8 = inception_block(pool4, [256, 160, 320, 32, 128, 128])
+    inception9 = inception_block(inception8, [384, 192, 384, 48, 128, 128])
 
     pool5 = K.layers.AveragePooling2D(
         (7, 7), strides=1, padding='valid'
