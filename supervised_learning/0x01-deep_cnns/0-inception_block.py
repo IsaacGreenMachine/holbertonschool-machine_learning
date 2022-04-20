54bd41d0e41ca4db252fb6c903332ad11454ba40
Isaac Green <zuderg@gmail.com>
2022-04-19 21:52:04 -0500

    adding fixed product

--- supervised_learning/0x01-deep_cnns/0-inception_block.py
+++ supervised_learning/0x01-deep_cnns/0-inception_block.py
@@ -18,6 +18,7 @@ def inception_block(A_prev, filters):
 
     Returns: the concatenated output of the inception block
     """
+    """
     init = K.initializers.he_normal()
     F1, F3R, F3, F5R, F5, FPP = filters
     # first group
@@ -74,3 +75,67 @@ def inception_block(A_prev, filters):
     # connect time
     concat = K.layers.Concatenate()([conv1, conv4, conv5, conv6])
     return concat
+    """
+
+    F1, F3R, F3, F5R, F5, FPP = filters
+    init = K.initializers.he_normal()
+
+    # 1D
+    conv1D = K.layers.Conv2D(
+        F1,
+        (1, 1),
+        padding='same',
+        activation='relu',
+        kernel_initializer=init
+    )(A_prev)
+
+    conv3_1D = K.layers.Conv2D(
+        F3R,
+        (1, 1),
+        padding='same',
+        activation='relu',
+        kernel_initializer=init
+    )(A_prev)
+
+    conv3 = K.layers.Conv2D(
+        F3,
+        (3, 3),
+        padding='same',
+        activation='relu',
+        kernel_initializer=init
+    )(conv3_1D)
+
+    # 1D -> 5x5
+    conv5_1D = K.layers.Conv2D(
+        F5R,
+        (1, 1),
+        padding='same',
+        activation='relu',
+        kernel_initializer=init
+    )(A_prev)
+
+    conv5 = K.layers.Conv2D(
+        F5,
+        (5, 5),
+        padding='same',
+        activation='relu',
+        kernel_initializer=init
+    )(conv5_1D)
+
+    # 3x3 pool -> 1D
+    pool = K.layers.MaxPooling2D(
+        (2, 2),
+        strides=(1, 1),
+        padding='same'
+    )(A_prev)
+    convPool = K.layers.Conv2D(
+        FPP,
+        (1, 1),
+        padding='same',
+        activation='relu',
+        kernel_initializer=init
+    )(pool)
+
+    out = K.layers.concatenate([conv1D, conv3, conv5, convPool])
+
+    return out
