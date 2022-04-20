1f19b06977a3fc8ee24402566ab4c39424417e23
Isaac Green <zuderg@gmail.com>
2022-04-19 22:31:51 -0500

    changing

--- supervised_learning/0x01-deep_cnns/1-inception_network.py
+++ supervised_learning/0x01-deep_cnns/1-inception_network.py
@@ -12,7 +12,7 @@ def inception_network():
 
     Returns: the keras model
     """
-
+    """
     # input size (224 x 224 x 3)
     input = K.Input(shape=(224, 224, 3))
     init = K.initializers.he_normal()
@@ -121,3 +121,57 @@ def inception_network():
         )(dropout_1)
 
     return K.Model(inputs=input, outputs=softmax_1)
+    """
+    init = K.initializers.he_normal()
+    input = K.Input(shape=(224, 224, 3))
+    conv1 = K.layers.Conv2D(
+        64, (7, 7), strides=(2, 2), padding='same',
+        activation='relu', kernel_initializer=init
+        )(input)
+
+    pool1 = K.layers.MaxPooling2D(
+        (3, 3), strides=(2, 2), padding='same'
+        )(conv1)
+
+    convE = K.layers.Conv2D(
+        64, kernel_size=(1, 1), padding='same', activation='relu',
+        kernel_initializer=init
+    )(pool1)
+
+    conv2 = K.layers.Conv2D(
+        192, (3, 3), activation='relu', padding='same',
+        kernel_initializer=init
+        )(convE)
+    pool2 = K.layers.MaxPooling2D(
+        (3, 3), strides=(2, 2), padding='same'
+        )(conv2)
+
+    inception1 = inception_block(pool2, (64, 96, 128, 16, 32, 32))
+    inception2 = inception_block(inception1, (128, 128, 192, 32, 96, 64))
+
+    pool3 = K.layers.MaxPooling2D(
+        (3, 3), strides=(2, 2), padding='same'
+        )(inception2)
+
+    inception3 = inception_block(pool3, (192, 96, 208, 16, 48, 64))
+    inception4 = inception_block(inception3, (160, 112, 224, 24, 64, 64))
+    inception5 = inception_block(inception4, (128, 128, 256, 24, 64, 64))
+    inception6 = inception_block(inception5, (112, 144, 288, 32, 64, 64))
+    inception7 = inception_block(inception6, (256, 160, 320, 32, 128, 128))
+
+    pool4 = K.layers.MaxPooling2D(
+        (3, 3), strides=(2, 2), padding='same'
+        )(inception7)
+
+    inception8 = inception_block(pool4, (256, 160, 320, 32, 128, 128))
+    inception9 = inception_block(inception8, (384, 192, 384, 48, 128, 128))
+
+    pool5 = K.layers.AveragePooling2D(
+        (7, 7), strides=(7, 7), padding='same'
+        )(inception9)
+    dropout = K.layers.Dropout(0.4)(pool5)
+    dense = K.layers.Dense(
+        1000, activation='softmax', kernel_initializer=init
+        )(dropout)
+
+    return K.Model(inputs=input, outputs=dense)
