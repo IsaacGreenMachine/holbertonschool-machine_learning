969a410517ffadb2e449decbaf7122c39bd00c38
Isaac Green <zuderg@gmail.com>
2022-06-07 18:01:56 -0500

    error handling

--- math/0x00-advanced_linear_algebra/4-inverse.py
+++ math/0x00-advanced_linear_algebra/4-inverse.py
@@ -4,83 +4,65 @@
 
 def inverse(matrix):
     """returns the inverse of a matrix"""
-    if (
-        matrix and matrix[0] and type(matrix) is list and
-        all(type(sub) is list for sub in matrix)
-       ):
-        width = len(matrix)
-        if matrix == [[]]:
-            raise ValueError("matrix must be a non-empty square matrix")
-        for height in matrix:
-            if len(height) != width:
-                raise ValueError("matrix must be a non-empty square matrix")
-
-    ################
-        adj = adjugate(matrix)
-        det = determinant(matrix)
-        if det:
-            inv_det = 1 / determinant(matrix)
-            for i in range(len(matrix)):
-                for j in range(len(matrix[i])):
-                    adj[i][j] *= inv_det
-            return(adj)
-        else:
-            return None
-    ################
-
-    else:
+    if type(matrix) is not list:
         raise TypeError("matrix must be a list of lists")
+    width = len(matrix)
+    if width == 0:
+        raise TypeError("matrix must be a list of lists")
+    for item in matrix:
+        if type(item) is not list:
+            raise TypeError("matrix must be a list of lists")
+        if len(item) != width:
+            raise ValueError("matrix must be a non-empty square matrix")
+    adj = adjugate(matrix)
+    det = determinant(matrix)
+    if det:
+        inv_det = 1 / determinant(matrix)
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                adj[i][j] *= inv_det
+        return(adj)
+    else:
+        return None
 
 
 def adjugate(matrix):
     """returns the adjugate of a matrix"""
-    if (
-        matrix and matrix[0] and type(matrix) is list and
-        all(type(sub) is list for sub in matrix)
-    ):
-        width = len(matrix)
-        if matrix == [[]]:
-            raise ValueError("matrix must be a non-empty square matrix")
-        for height in matrix:
-            if len(height) != width:
-                raise ValueError("matrix must be a non-empty square matrix")
-    ################
-        return(matrix_transpose(cofactor(matrix)))
-    ################
-    else:
+    if type(matrix) is not list:
         raise TypeError("matrix must be a list of lists")
+    width = len(matrix)
+    if width == 0:
+        raise TypeError("matrix must be a list of lists")
+    for item in matrix:
+        if type(item) is not list:
+            raise TypeError("matrix must be a list of lists")
+        if len(item) != width:
+            raise ValueError("matrix must be a non-empty square matrix")
+    return(matrix_transpose(cofactor(matrix)))
 
 
 def cofactor(matrix):
     """returns the cofactor of a matrix"""
-    if (
-        matrix and matrix[0] and type(matrix) is list and
-        all(type(sub) is list for sub in matrix)
-    ):
-        width = len(matrix)
-        if matrix == [[]]:
-            raise ValueError("matrix must be a non-empty square matrix")
-        for height in matrix:
-            if len(height) != width:
-                raise ValueError("matrix must be a non-empty square matrix")
-
-    ################
-        new_mat = minor(deepcopy(matrix))
-        modfirst = 1
-        modcurr = 1
-        for row in range(len(matrix)):
-            for col in range(len(matrix[row])):
-                # print(matrix[row][col], end=' ')
-                new_mat[row][col] *= (modfirst * modcurr)
-                modcurr *= -1
-            modfirst *= -1
-            modcurr = 1
-            # print()
-        return new_mat
-    ################
-
-    else:
+    if type(matrix) is not list:
         raise TypeError("matrix must be a list of lists")
+    width = len(matrix)
+    if width == 0:
+        raise TypeError("matrix must be a list of lists")
+    for item in matrix:
+        if type(item) is not list:
+            raise TypeError("matrix must be a list of lists")
+        if len(item) != width:
+            raise ValueError("matrix must be a non-empty square matrix")
+    new_mat = minor(deepcopy(matrix))
+    modfirst = 1
+    modcurr = 1
+    for row in range(len(matrix)):
+        for col in range(len(matrix[row])):
+            new_mat[row][col] *= (modfirst * modcurr)
+            modcurr *= -1
+        modfirst *= -1
+        modcurr = 1
+    return new_mat
 
 
 def deepcopy(matrix):
@@ -95,21 +77,25 @@ def deepcopy(matrix):
 
 def determinant(matrix):
     """returns the determinant of a given matrix"""
-    if matrix == [[]]:
-        return 1
-    if (
-        matrix and matrix[0] and type(matrix) is list
-        and all(type(sub) is list for sub in matrix)
-    ):
-        width = len(matrix)
-        for height in matrix:
-            if len(height) != width:
-                raise ValueError("matrix must be a square matrix")
-
-    ################
-        if width == 1:
+    if type(matrix) is not list:
+        raise TypeError("matrix must be a list of lists")
+    width = len(matrix)
+    if width == 0:
+        raise TypeError("matrix must be a list of lists")
+    for item in matrix:
+        if type(item) is not list:
+            raise TypeError("matrix must be a list of lists")
+        if len(item) != width and width != 1:
+            raise ValueError("matrix must be a non-empty square matrix")
+    if width == 1:
+        if len(matrix[0]) == 0:
+            raise 1
+        elif len(matrix[0]) == 1:
             return matrix[0][0]
-        elif width == 2:
+        else:
+            raise ValueError("matrix must be a non-empty square matrix")
+    else:
+        if width == 2:
             return (
                 (matrix[0][0] * matrix[1][1]) -
                 (matrix[0][1] * matrix[1][0])
@@ -130,47 +116,40 @@ def determinant(matrix):
                 else:
                     det -= (matrix[0][i] * determinant(sub_mat))
             return det
-    ################
-
-    else:
-        raise TypeError("matrix must be a list of lists")
 
 
 def minor(matrix):
     """returns the minor of a matrix"""
-    if (
-            matrix and matrix[0] and type(matrix) is list and
-            all(type(sub) is list for sub in matrix)):
-        width = len(matrix)
-        if matrix == [[]]:
-            raise ValueError("matrix must be a non-empty square matrix")
-        for height in matrix:
-            if len(height) != width:
-                raise ValueError("matrix must be a non-empty square matrix")
-    ################
-        if width == 1:
-            return [[1]]
-        elif width == 2:
-            return (
-                [[int(matrix[1][1]), int(matrix[1][0])],
-                 [int(matrix[0][1]), int(matrix[0][0])]]
-            )
-        else:
-            new_mat = deepcopy(matrix)
-            for i in range(len(matrix[0])):
-                for j in range(len(matrix[0])):
-                    sub_mat = []
-                    for row in range(len(matrix[0])):
-                        sub_mat.append([])
-                        for col in range(len(matrix[0])):
-                            if row != i and col != j:
-                                sub_mat[row].append(int(matrix[row][col]))
-                    sub_mat = [x for x in sub_mat if x != []]
-                    new_mat[i][j] = determinant(sub_mat)
-            return new_mat
-    ################
-    else:
+    if type(matrix) is not list:
         raise TypeError("matrix must be a list of lists")
+    width = len(matrix)
+    if width == 0:
+        raise TypeError("matrix must be a list of lists")
+    for item in matrix:
+        if type(item) is not list:
+            raise TypeError("matrix must be a list of lists")
+        if len(item) != width:
+            raise ValueError("matrix must be a non-empty square matrix")
+    if width == 1:
+        return [[1]]
+    elif width == 2:
+        return (
+          [[int(matrix[1][1]), int(matrix[1][0])],
+           [int(matrix[0][1]), int(matrix[0][0])]]
+          )
+    else:
+        new_mat = deepcopy(matrix)
+        for i in range(len(matrix[0])):
+            for j in range(len(matrix[0])):
+                sub_mat = []
+                for row in range(len(matrix[0])):
+                    sub_mat.append([])
+                    for col in range(len(matrix[0])):
+                        if row != i and col != j:
+                            sub_mat[row].append(int(matrix[row][col]))
+                sub_mat = [x for x in sub_mat if x != []]
+                new_mat[i][j] = determinant(sub_mat)
+        return new_mat
 
 
 def matrix_transpose(matrix):
