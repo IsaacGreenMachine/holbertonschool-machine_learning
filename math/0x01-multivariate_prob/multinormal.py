420c70dced3ec9430e8a2324d4407cb309c492d9
Isaac Green <zuderg@gmail.com>
2022-06-09 21:55:17 -0500

    changing error check

--- math/0x01-multivariate_prob/multinormal.py
+++ math/0x01-multivariate_prob/multinormal.py
@@ -8,7 +8,7 @@ class MultiNormal():
         """initializes mean and covariance matrix of class"""
         if type(data) is not np.ndarray or len(data.shape) != 2:
             raise TypeError("data must be a 2D numpy.ndarray")
-        if data.shape[1] < 2:
+        if data.shape[0] < 2:
             raise ValueError("data must contain multiple data points")
         mean, cov = self.mean_cov(data.T)
         self.mean = mean.T
