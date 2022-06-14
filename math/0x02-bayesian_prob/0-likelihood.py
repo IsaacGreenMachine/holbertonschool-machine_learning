b3723644568d18e067239be4e3d8cdd06612293e
Isaac Green <zuderg@gmail.com>
2022-06-14 14:22:19 -0500

    changing if checks

--- math/0x02-bayesian_prob/0-likelihood.py
+++ math/0x02-bayesian_prob/0-likelihood.py
@@ -22,15 +22,15 @@ def likelihood(x, n, P):
     Returns - 1D numpy.ndarray containing the likelihood of obtaining the data,
               x and n, for each probability in P, respectively
     """
-    if type(n) != int or n < 0:
+    if type(n) is not int or n <= 0:
         raise ValueError("n must be a positive integer")
-    if type(x) != int or x < 0:
+    if type(x) is not int or x < 0:
         raise ValueError(
             "x must be an integer that is greater than or equal to 0"
             )
     if x > n:
         raise ValueError("x cannot be greater than n")
-    if type(P) != np.ndarray or len(P.shape) != 1:
+    if type(P) is not np.ndarray or len(P.shape) != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
     if not (all(P <= 1) and all(P >= 0)):
         raise ValueError("All values in P must be in the range [0, 1]")
