d15398e64062813080ea78025a46096bfd9b52a6
Isaac Green <zuderg@gmail.com>
2022-06-14 14:51:00 -0500

    pycodestyle

--- math/0x02-bayesian_prob/1-intersection.py
+++ math/0x02-bayesian_prob/1-intersection.py
@@ -25,7 +25,7 @@ def intersection(x, n, P, Pr):
     if type(x) is not int or x < 0:
         raise ValueError(
             "x must be an integer that is greater than or equal to 0"
-            )
+        )
     if x > n:
         raise ValueError("x cannot be greater than n")
     if type(P) is not np.ndarray or len(P.shape) != 1:
@@ -44,12 +44,12 @@ def likelihood(x, n, P):
     calculates the likelihood of obtaining this data given various
     hypothetical probabilities of developing severe side effects
     """
-    if type(n) is not int or n<=0:
+    if type(n) is not int or n <= 0:
         raise ValueError("n must be a positive integer")
-    if type(x) is not int or x<0:
+    if type(x) is not int or x < 0:
         raise ValueError(
             "x must be an integer that is greater than or equal to 0"
-            )
+        )
     if x > n:
         raise ValueError("x cannot be greater than n")
     if type(P) is not np.ndarray or len(P.shape) != 1:
@@ -64,4 +64,4 @@ def nCr(n, r):
     """returns the number of combinations with n choices and r possibilities"""
     return (
         np.math.factorial(n)/(np.math.factorial(r)*np.math.factorial(n - r))
-        )
+    )
