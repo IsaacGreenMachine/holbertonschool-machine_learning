23589e6962fc66724e5f262211c1cbc8ce1d3e21
Isaac Green <zuderg@gmail.com>
2022-06-14 14:55:54 -0500

    sum if check

--- math/0x02-bayesian_prob/1-intersection.py
+++ math/0x02-bayesian_prob/1-intersection.py
@@ -32,10 +32,12 @@ def intersection(x, n, P, Pr):
         raise TypeError("P must be a 1D numpy.ndarray")
     if type(Pr) is not np.ndarray or Pr.shape != P.shape:
         raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
-    if True in ((P < 0) + (P > 1)):
+    if np.count_nonzero((P < 0) | (P > 1)) > 0:
         raise ValueError("All values in P must be in the range [0, 1]")
-    if True in ((Pr < 0) + (Pr > 1)):
+    if np.count_nonzero((Pr < 0) | (Pr > 1)) > 0:
         raise ValueError("All values in Pr must be in the range [0, 1]")
+    if np.sum(Pr) != 1:
+        raise ValueError("Pr must sum to 1")
     return(Pr * likelihood(x, n, P))
 
 
