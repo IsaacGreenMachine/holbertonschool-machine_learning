5266362910c2704f10f6a9c51c9835e33b1f2a71
Isaac Green <zuderg@gmail.com>
2022-06-14 14:49:52 -0500

    fixing if checks

--- math/0x02-bayesian_prob/1-intersection.py
+++ math/0x02-bayesian_prob/1-intersection.py
@@ -33,17 +33,9 @@ def intersection(x, n, P, Pr):
     if type(Pr) is not np.ndarray or Pr.shape != P.shape:
         raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
     if np.count_nonzero((P < 0) | (P > 1)) > 0:
-        raise ValueError(
-            "All values in {} must be in the range [0, 1]".format(
-                P[((P < 0) | (P > 1)).argmax()]
-                )
-                )
+        raise ValueError("All values in P must be in the range [0, 1]")
     if np.count_nonzero((Pr < 0) | (Pr > 1)) > 0:
-        raise ValueError(
-            "All values in {} must be in the range [0, 1]".format(
-                Pr[((Pr < 0) | (Pr > 1)).argmax()]
-                )
-                )
+        raise ValueError("All values in Pr must be in the range [0, 1]")
     return(Pr * likelihood(x, n, P))
 
 
@@ -52,9 +44,9 @@ def likelihood(x, n, P):
     calculates the likelihood of obtaining this data given various
     hypothetical probabilities of developing severe side effects
     """
-    if type(n) is not int or n <= 0:
+    if type(n) is not int or n<=0:
         raise ValueError("n must be a positive integer")
-    if type(x) is not int or x < 0:
+    if type(x) is not int or x<0:
         raise ValueError(
             "x must be an integer that is greater than or equal to 0"
             )
