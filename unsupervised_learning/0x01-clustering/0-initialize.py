74b02b374246271aa329db0124a0652106934374
Isaac Green <zuderg@gmail.com>
2022-06-27 21:51:11 -0500

    adding README and task 0

---
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""module for the initialize function"""
+import numpy as np
+
+
+def initialize(X, k):
+    """
+    initializes cluster centroids for K-means:
+
+    X - numpy.ndarray of shape (n, d)
+        containing the dataset that will be used for K-means clustering
+        n - number of data points
+        d is the number of dimensions for each data point
+    k - positive integer containing the number of clusters
+
+    centroids are initialized with a multivariate uniform
+    distribution along each dimension in d
+
+    The min/max values for the distribution
+    are min/max values of X along each dimension in d
+
+    Returns: a numpy.ndarray of shape (k, d) containing
+        the initialized centroids for each cluster, or None on failure
+    """
+    clusters = np.random.uniform(X.min(axis=0), X.max(axis=0), (k, X.shape[1]))
+    return clusters
---
+++ unsupervised_learning/0x01-clustering/README.md
@@ -0,0 +1,1 @@
+repo to learn about k-means clustering, multimodal gaussian distributions, and agglomerative clustering at holberton school
