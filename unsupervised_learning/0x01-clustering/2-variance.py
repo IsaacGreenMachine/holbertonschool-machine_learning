8ef39365178cc7acc7c54ca39147d19daedffbab
Isaac Green <zuderg@gmail.com>
2022-06-27 22:07:31 -0500

    adding task

---
+++ unsupervised_learning/0x01-clustering/2-variance.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""
+Module for variance function
+"""
+
+
+import numpy as np
+
+
+def Kassignments(X, clusters):
+    """
+    Returns list of indexes corresponding to
+    min euclidean distance of each point in X to
+    each point in clusters.
+    """
+    points, dims = X.shape
+    x = X.reshape(points, 1, dims)
+    x = x - np.repeat(clusters[np.newaxis, ...], points, axis=0)
+    dist = np.linalg.norm(x, axis=2)
+
+    mins = np.argmin(dist, axis=1)
+    return mins
+
+
+def variance(X, C):
+    """
+    Calculates the total intra-cluster variance
+      for a dataset.
+    Args:
+        X: numpy.ndarray - (n, d) containing the data set.
+        C: numpy.ndarray - (k, d) containing the centroid
+          means for each cluster.
+    Return:
+        Variance, or None on failure.
+    """
+
+    if type(X) is not np.ndarray or X.ndim != 2:
+        return None
+    if type(C) is not np.ndarray or C.ndim != 2:
+        return None
+    if C.shape[1] != X.shape[1]:
+        return None
+
+    if C.shape[0] == 1:
+        return ((X-np.mean(X, axis=0))**2).sum()
+
+    assignments = Kassignments(X, C)
+    Karanged = C[assignments]
+
+    return ((X-Karanged)**2).sum()
