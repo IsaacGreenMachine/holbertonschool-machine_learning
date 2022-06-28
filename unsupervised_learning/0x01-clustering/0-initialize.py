f6f66ec5d5d0b51ee831a82e9ca85d46436b5dee
Isaac Green <zuderg@gmail.com>
2022-06-27 22:00:14 -0500

    if checks

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -23,7 +23,7 @@ def initialize(X, k):
         the initialized centroids for each cluster, or None on failure
     """
     if (type(X) != np.ndarray or len(X.shape) != 2 or
-            type(k) != int or k > X.shape[0]):
+            type(k) != int or k > X.shape[0] or k < 1):
         return None
     clusters = np.random.uniform(X.min(axis=0), X.max(axis=0), (k, X.shape[1]))
     return clusters
