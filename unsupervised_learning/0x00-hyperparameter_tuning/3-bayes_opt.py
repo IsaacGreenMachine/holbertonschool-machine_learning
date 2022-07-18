8ccce54a96fc59ed17b5d41344c34fc4e329cb6e
Isaac Green <zuderg@gmail.com>
2022-07-18 14:42:41 -0500

    pycodestyle

--- unsupervised_learning/0x00-hyperparameter_tuning/3-bayes_opt.py
+++ unsupervised_learning/0x00-hyperparameter_tuning/3-bayes_opt.py
@@ -10,8 +10,7 @@ class BayesianOptimization():
     """
     def __init__(
             self, f, X_init, Y_init, bounds, ac_samples, l=1,
-            sigma_f=1, xsi=0.01, minimize=True
-                ):
+            sigma_f=1, xsi=0.01, minimize=True):
         """
         f is the black-box function to be optimized
 
