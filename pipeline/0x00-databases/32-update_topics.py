c2248515b3aad45d90c1271cfcd6fcaa195c1a3e
Isaac Green <zuderg@gmail.com>
2022-11-30 16:46:08 -0600

    initial push

---
+++ pipeline/0x00-databases/32-update_topics.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" module for update_topics function """
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    '''
+    changes all topics of a school document based on the name
+    mongo_collection will be the pymongo collection object
+    name (string) will be the school name to update
+    topics (list of strings) will be
+        the list of topics approached in the school
+    '''
+    mongo_collection.update_many({name: topics})
