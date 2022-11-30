8114a576e3e1b2a15978999f6a187322bde7206a
Isaac Green <zuderg@gmail.com>
2022-11-30 16:49:52 -0600

    initial push

--- pipeline/0x00-databases/32-update_topics.py
+++ pipeline/0x00-databases/32-update_topics.py
@@ -11,4 +11,5 @@ def update_topics(mongo_collection, name, topics):
     topics (list of strings) will be
         the list of topics approached in the school
     '''
-    mongo_collection.update_many({name: topics})
+    mongo_collection.update_many({"name": name},
+                                 {'$set': {'name': name, 'topics': topics}})
