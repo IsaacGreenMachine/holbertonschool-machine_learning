080375a6d04839eff1d1563c5870156e8a27e0e5
Isaac Green <zuderg@gmail.com>
2022-11-29 22:41:50 -0600

    ordering by rating DESC

--- pipeline/0x00-databases/12-rating_genres.sql
+++ pipeline/0x00-databases/12-rating_genres.sql
@@ -8,5 +8,5 @@ LEFT JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 LEFT JOIN tv_show_ratings
 ON tv_show_ratings.show_id = tv_show_genres.show_id
-GROUP BY tv_genres.name;
--- genre_name -> genre_id ->  rating
+GROUP BY tv_genres.name
+ORDER BY rating DESC;
