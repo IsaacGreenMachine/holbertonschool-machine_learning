ccbf5a661832dcf85b6910f1403907fbb21b9005
Isaac Green <zuderg@gmail.com>
2022-11-29 22:38:28 -0600

    capitalizing SQL keywords

--- pipeline/0x00-databases/9-no_genre.sql
+++ pipeline/0x00-databases/9-no_genre.sql
@@ -1,7 +1,7 @@
 -- lists all shows contained in hbtn_0d_tvshows without a genre linked.
 -- display format: tv_shows.title - tv_show_genres.genre_id
 
-SELECT title, tv_show_genres.genre_id from tv_shows
+SELECT title, tv_show_genres.genre_id FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NULL
