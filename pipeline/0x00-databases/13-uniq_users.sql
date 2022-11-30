09ed5f8d76b0cb90717d2b68f050c7948f9bf216
Isaac Green <zuderg@gmail.com>
2022-11-29 22:45:00 -0600

    changing primary key format

--- pipeline/0x00-databases/13-uniq_users.sql
+++ pipeline/0x00-databases/13-uniq_users.sql
@@ -4,7 +4,8 @@
 -- name, string (255 characters)
 
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     email CHAR(255) NOT NULL UNIQUE,
     name, CHAR(255),
+    PRIMARY KEY (id)
 );
