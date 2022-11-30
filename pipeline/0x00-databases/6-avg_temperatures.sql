0f71f95da9e7f96744ee53afdf61365055c3f4ae
Isaac Green <zuderg@gmail.com>
2022-11-29 22:36:44 -0600

    renaming alias for average

--- pipeline/0x00-databases/6-avg_temperatures.sql
+++ pipeline/0x00-databases/6-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- displays the average temperature (Fahrenheit) grouped by city in descending temperature order
-SELECT city, AVG(value) as average FROM temperatures GROUP BY city ORDER BY AVG(value) DESC;
+SELECT city, AVG(value) as avg_temp FROM temperatures GROUP BY city ORDER BY AVG(value) DESC;
