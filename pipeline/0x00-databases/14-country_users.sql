28ac737df2c6d35b251e3ba4327872706ba1ff82
Isaac Green <zuderg@gmail.com>
2022-11-29 22:49:08 -0600

    fixing primary key and adding default country

--- pipeline/0x00-databases/14-country_users.sql
+++ pipeline/0x00-databases/14-country_users.sql
@@ -4,8 +4,9 @@
 --   name, string (255 characters)
 --   country, enumeration of countries: US, CO and TN, never null (= default will be the first element of the enumeration, here US)
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO-INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO-INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN') NOT NULL,
+    country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL,
+    PRIMARY KEY(id)
 )
